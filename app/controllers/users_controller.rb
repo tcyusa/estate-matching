aaf628865d2ef2effa3009b2b674a688b73105f5
tcyu.sa0618 <tcyu.sa0618@gmail.com>
2019-07-02 04:31:20 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    set_user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # 今は全て一般ユーザ
+    @user.is_estate = false
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザー登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
