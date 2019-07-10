127151295fffa69cdb8c0516b5e26f3f13b7b84e
tcyu.sa0618 <tcyu.sa0618@gmail.com>
2019-07-10 01:35:07 +0000

    dm to user

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,12 +9,17 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find(params[:id])
-    if (@entry.estate_user_id == current_user.id || @entry.normal_user_id == current_user.id)
-      @messages = @entry.messages
-      @message = Message.new
+    if (@entry.estate_user_id == current_user.id)
+      @opponent = User.find(@entry.normal_user_id)
+    elsif (@entry.normal_user_id == current_user.id)
+      @opponent = User.find(@entry.estate_user_id)
     else
       redirect_back(fallback_location: root_path)
     end
+    
+#    if (@entry.estate_user_id == current_user.id || @entry.normal_user_id == current_user.id)
+    @messages = @entry.messages
+    @message = Message.new
   end
 
 end
